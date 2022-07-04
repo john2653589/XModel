522d9624d2fa9da48871600367604085c8cfc32d
Rugal Tu <john2653589@gmail.com>
2022-07-04 20:53:11 +0800

    Rugal Update

--- XModelProject/XModelProject/Rugal/XModel/XModelProperty.cs
+++ XModelProject/XModelProject/Rugal/XModel/XModelProperty.cs
@@ -4,5 +4,6 @@ namespace Rugal.Xamarin.XModel.BindingProperty
     public class XModelProperty
     {
         public const string Text = "TextProperty";
+        public const string ItemsSource = "ItemsSourceProperty";
     }
 }
